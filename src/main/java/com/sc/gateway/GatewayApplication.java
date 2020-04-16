c74b688ca5b321fe45486398e0a00dca8a8e9d03
freshJuice <1326191992@qq.com>
2020-04-17 00:03:51 +0800

    init

---
+++ src/main/java/com/sc/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.sc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@RefreshScope
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] argv) {
+        SpringApplication.run(GatewayApplication.class, argv);
+    }
+}
